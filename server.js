d84f1070a98879f9697cc5e593de779b35b80da8
Khurram Virani <kvirani@gmail.com>
2016-07-15 16:48:34 -0700

    Added knex and express logging

--- server.js
+++ server.js
@@ -11,8 +11,19 @@ const app         = express();
 
 const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
+const morgan      = require('morgan');
+const knexLogger  = require('knex-logger');
 
+// Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
+
+// Load the logger first so all (static) HTTP requests are logged to STDOUT
+// 'dev' = Concise output colored by response status for development use.
+//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+app.use(morgan('dev'));
+
+// Log knex SQL queries to STDOUT as well
+app.use(knexLogger(knex));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,7 +35,7 @@ app.use("/styles", sass({
 }));
 app.use(express.static("public"));
 
-// Users JSON api
+// Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
 
 // Home page
