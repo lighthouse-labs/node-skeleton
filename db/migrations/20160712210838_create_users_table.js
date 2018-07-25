5f246f3654335c22547e15f7b92aba918e0862bf
Sai Gautam <sai.gautam@gmail.com>
2018-07-25 20:41:18 +0000

    Added KNEX Step to create Database Tables

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,5 +1,5 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function (table) {
+  return knex.schema.createTable('todo_users', function (table) {
     table.increments();
     table.string('name');
   });
