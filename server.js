8ce5367d3f654fd1df9b17ea377cb14239b1cba2
Francis Bourgouin <netbeuix@gmail.com>
2021-10-14 09:31:09 -0400

    small cleanup + fix for script

--- server.js
+++ server.js
@@ -1,48 +1,39 @@
 // load .env data into process.env
-require('dotenv').config();
+require("dotenv").config();
 
 // Web server config
 const PORT = process.env.PORT || 8080;
 const ENV = process.env.ENV || "development";
-const fs = require("fs")
+const fs = require("fs");
 const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 
 // PG database client/connection setup
-const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
 db.connect();
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get("/styles/:css_file", (req, res, next) => {
-//   const cssFilename = req.params.css_file.replace(/\.[^/.]+$/, "")
-//   console.log(cssFilename)
-//   const rendered = sass.renderSync({
-//     file: `${__dirname}/styles/${cssFilename}.scss`,
-//     outFile: `${__dirname}/public/styles/${cssFilename}.css`,
-//     debug: true,
-//     outputStyle: 'expanded'
-//   })
-//   fs.writeFileSync(`${__dirname}/public/styles/${cssFilename}.css`, rendered.css.toString())
-//   next()
-// });
+app.use(
+  "/styles",
+  sassMiddleware({
+    source: __dirname + "/styles",
+    destination: __dirname + "/public/styles",
+    isSass: false, // false => scss, true => sass
+  })
+);
 
-app.use("/styles", sassMiddleware({
-  source: __dirname + "/styles",
-  destination: __dirname + "/public/styles",
-  isSass: true
-}))
 app.use(express.static("public"));
 
 // Separated Routes for each Resource
@@ -59,7 +50,6 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-
 
 app.get("/", (req, res) => {
   res.render("index");
