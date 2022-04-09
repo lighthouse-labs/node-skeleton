1093ba619b31080325c23311bd811da951173f12
amuliawijaya23 <amuliawijaya@gmail.com>
2022-04-09 13:15:37 -0700

    add filter options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,20 @@
 // Client facing scripts here
+$(document).ready(function() {
+
+  $('#carMake').change(function() {
+    const make = $(this).val();
+
+    $('#carModel option').each(function() {
+      if ($(this).data('tag') !== make) {
+        $(this).hide();
+      } else {
+        $(this).show();
+      }
+    });
+  });
+
+  $('.filter').click(function() {
+    $('.filterOptions').slideToggle('fast');
+    $('.filterOptions').css('display', 'flex')
+  })
+});
