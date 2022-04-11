b17b0bf240f3b952eb101d3cebc752c41258f1eb
Alvin <alvin.tol@hotmail.com>
2022-04-11 19:57:37 +0000

    adds getChat function to start generating chat box between buyer/seller

--- server/database.js
+++ server/database.js
@@ -3,7 +3,7 @@ const dbParams = require("../lib/db");
 const db = new Pool(dbParams);
 db.connect();
 
-const getAllListings = function(limit) {
+const getAllListings = function (limit) {
   return db.query(`SELECT *
     FROM listings
     ORDER BY listings DESC
@@ -13,7 +13,7 @@ const getAllListings = function(limit) {
 };
 
 
-const getMessages = function() {
+const getInboxNames = () => {
   return db.query(`SELECT users.name AS name, sender_id, COUNT(*) AS num_of_messages
   FROM messagelisting 
   JOIN users 
@@ -25,15 +25,23 @@ const getMessages = function() {
     .catch((err) => console.log(err.message));
 };
 
+const getChat = () => {
+  return db.query(`SELECT *                                     
+  FROM messagelisting
+  ORDER BY id;
+    `)
+    .then((result) => result.rows)
+    .catch((err) => console.log(err.message));
+}
 
-const getFavorites = function(favorites) {
+const getFavorites = function (favorites) {
 
 };
 
 
 
 
-const createListing = function(listings) {
+const createListing = function (listings) {
   const queryParams = [
     listings.price,
     listings.year,
@@ -57,20 +65,20 @@ const createListing = function(listings) {
     ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`;
 
   return db.query(queryString, queryParams)
-  .then((res) => res.rows)
-  .catch((err) => console.log(err.message));
+    .then((res) => res.rows)
+    .catch((err) => console.log(err.message));
 };
 
-const getAllMakes = function() {
+const getAllMakes = function () {
   return db.query(`SELECT make FROM listings;`)
-  .then((res) => res.rows)
-  .catch((err) => console.log(err.message));
+    .then((res) => res.rows)
+    .catch((err) => console.log(err.message));
 };
 
-const getAllModels = function() {
+const getAllModels = function () {
   return db.query(`SELECT model FROM listings;`)
-  .then((res) => res.rows)
-  .catch((err) =>  console.log(err.message));
+    .then((res) => res.rows)
+    .catch((err) => console.log(err.message));
 };
 
 module.exports = {
@@ -78,5 +86,5 @@ module.exports = {
   createListing,
   getAllMakes,
   getAllModels,
-  getMessages
-}
+  getInboxNames
+};
