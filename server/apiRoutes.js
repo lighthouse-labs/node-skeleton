730eb2ebe7d213880984d6683412a5245da085c4
Alastair <alastair05@gmail.com>
2022-04-11 08:41:24 -0700

    Add function for new listings cancel button

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -5,33 +5,33 @@ const database = require('./database');
 
 
 
-  router.get('', (req, res) => {
-    database.getAllListings(10)
-      .then(listings => res.json(listings))
-      .catch(e => {
-        console.error(e);
-        res.send(e);
-      });
-  });
+router.get('', (req, res) => {
+  database.getAllListings(10)
+    .then(listings => res.json(listings))
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    });
+});
 
 
-  router.post('', (req, res) => {
-    const form = req.body;
-    if (!form.model || !form.make || !form.year || !form.price || !form.color)  {
-      res.status(403).send('Missing Required Information!');
-      return;
-    }
+router.post('', (req, res) => {
+  const form = req.body;
+  if (!form.model || !form.make || !form.year || !form.price || !form.color)  {
+    res.status(403).send('Missing Required Information!');
+    return;
+  }
 
-    database.createListing(req.body)
-      .then(listing => {
-        console.log(req.body, "\nListing Added to Databse")
-        res.status(201).send('New Listing Created!');
-      })
-      .catch(e => {
-        console.error(e);
-        res.send(e);
-      });
-  });
+  database.createListing(req.body)
+    .then(listing => {
+      console.log(req.body, "\nListing Added to Databse");
+      res.status(201).send('New Listing Created!');
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    });
+});
 
 
-  module.exports = router;
+module.exports = router;
