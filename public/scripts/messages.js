f152b1142f6befe88aa01591cc51955d1661c0e7
Alvin <alvin.tol@hotmail.com>
2022-04-11 18:13:21 +0000

    adds script for rendering html for messages in database to inbox

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -1,5 +1,27 @@
 $(() => {
 
+  // Generates messages in inbox
+  const createMessages = (message) => {
+    
+    let $message = $(`
+    <div class="message">${message.user_id}</div>
+    `);
+    return $message;
+  };
+
+  // Renders messsages into the inbox
+  const renderMessages = () => {
+    $.get('/').then(data => {
+      $('.inbox').empty();
+
+      data.forEach(message => {
+        $('.inbox').prepend(createMessages(message));
+      });
+    });
+  };
+
+  renderMessages();
+
   // Messages inbox dropdown
   $('#messages').click(() => {
     $('.inbox').slideToggle('fast');
