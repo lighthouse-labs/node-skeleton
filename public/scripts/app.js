8d6c75dd35bbdefb1c8ea361a4d8a775a527913b
amuliawijaya23 <amuliawijaya@gmail.com>
2022-04-10 23:32:26 -0700

    adding select2 and updating search filter style

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,6 @@
 // Client facing scripts here
 $(document).ready(function() {
 
-  $('#carMake').change(function() {
-    const make = $(this).val();
-    $('#carModel option').each(function() {
-      if ($(this).data('tag') !== make) {
-        $(this).hide();
-      } else {
-        $(this).show();
-      }
-    });
-  });
 
   $('.filter').click(function() {
     $('.filterOptions').slideToggle('fast');
@@ -23,5 +13,23 @@ $(document).ready(function() {
     $('.newForm').css('display', 'flex');
     $('.filterOptions').slideUp('fast');
   });
+
+  $('#carMake').select2({});
+  $('#carModel').select2({});
+  $('#carTransmission').select2();
+
+  $('#carMake').change(function() {
+    const make =  $(this).val();
+    console.log('hello!');
+
+    $('#carModel option').each(function() {
+      if ($(this).data('tag') !== make) {
+        $(this).hide();
+      } else {
+        $(this).show();
+      }
+    });
+  });
+
 
 });
