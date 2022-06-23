7daea99442068c3ed5cc4b83eeeaff68b9f837e2
Kyler <kylerlepp8@gmail.com>
2022-06-23 01:04:46 -0700

    created the wine listing card layout for all three wine pages

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("all-wines");
 });
 
 app.listen(PORT, () => {
