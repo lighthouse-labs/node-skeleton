0749432a39d22e72ca3fc6a553480c5ee8dda8a3
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-06-25 17:14:01 -0700

    Set up db.js file

---
+++ db/db.js
@@ -0,0 +1,8 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+});
