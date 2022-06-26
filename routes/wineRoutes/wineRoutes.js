259ea267b1baa927b19937561b47ac1007bd07ce
Kyler <kylerlepp8@gmail.com>
2022-06-26 15:10:40 -0700

    added the database and included the queries to make all the reds load on the reds page and make all the whites load on the whites page

--- routes/wineRoutes/wineRoutes.js
+++ routes/wineRoutes/wineRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router  = express.Router();
 
 const { getAllWines } = require('../../db/database');
-
+const { getAllRedWines } = require('../../db/database');
+const { getAllWhiteWines } = require('../../db/database');
 
 module.exports = (db) => {
   router.get("/wines", async(req, res) => {
@@ -12,11 +13,19 @@ module.exports = (db) => {
     };
     res.render('all-wines', templateVars);
   });
-  router.get("/wines/red", function(req, res) {
-    res.render('red-wines');
+  router.get("/wines/red", async(req, res) => {
+    const result = await getAllRedWines();
+    const templateVars = {
+      allRedWines: result
+    };
+    res.render('red-wines', templateVars);
   });
-  router.get("/wines/white", function(req, res) {
-    res.render('white-wines');
+  router.get("/wines/white", async(req, res) => {
+    const result = await getAllWhiteWines();
+    const templateVars = {
+      allWhiteWines: result
+    };
+    res.render('white-wines', templateVars);
   });
   //Test db connection
   // router.get("/test", async (req, res) => {
